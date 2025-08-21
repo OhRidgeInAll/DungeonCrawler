b6ab1a9b777ad0ae8435a1dc7412c8250b4270c0
OhRidgeInAll <8bitOriginal@gmail.com>
2025-08-21 00:25:43 -0400

    Enemy Class

---
+++ Enemy.py
@@ -0,0 +1,17 @@
+from ursina import *
+from constants import *
+
+class Enemy(Actor):
+    def __init__(self, grid_x, grid_y):
+        super().__init__(
+            model='quad',
+            color=color.red,
+            scale=(TILE_SIZE * 0.8, TILE_SIZE * 0.8),
+            position = grid_to_world(grid_x, grid_y),
+            team = 1
+        )
+        self.grid_x = grid_x  # Integer grid column
+        self.grid_y = grid_y  # Integer grid row
+        self.move_cooldown = 1.0
+        self.attack_power = 8
+        #self.on_death = Death Behaviour Goes Here
